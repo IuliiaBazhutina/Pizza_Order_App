061ebaf95554917300482a720dd1d862505c292c
raeoffrost <raeoffrost@gmail.com>
2025-04-01 01:52:46 -0500

    improved pizza layering and connected it to pizza Context

--- components/pizza.js
+++ components/pizza.js
@@ -1,16 +1,58 @@
-import { useState } from "react";
+import { usePizza } from "./PizzaContext";
 import Toppings from "./toppings";
 import styles from "@/styles/Pizza.module.css";
 
-const Pizza = ({ isOnPizza }) => {
-  const [onPizza, setOnPizza] = useState([]);
+const Pizza = () => {
+  const { selectedToppings } = usePizza();
 
-  // const filterToppings = isOnPizza ? toppinglist.filter((img) => img.onPizza) : Toppings;
+  // filter to select specific toppings by id
+  // for setting default selections and proper layering
+  const defaultToppings = selectedToppings.map((item) => item.id);
+
   return (
     <div>
-      {Toppings.toReversed().map(({ name, src, id }) => (
-        <img key={id.id} src={src.src} alt={name} className={styles.img} />
-      ))}
+      {
+        // Require a crust, default to classic if none selected //
+        // Crust on bottom //
+        (defaultToppings.includes(11) ||
+          defaultToppings.includes(12) ||
+          defaultToppings.length === 0) && (
+          <img
+            src={defaultToppings.includes(12) ? Toppings[11].src.src : Toppings[10].src.src}
+            alt="Crust"
+            className={styles.img}
+          />
+        )
+      }
+
+      {
+        // Sauce layer next //
+        defaultToppings.includes(9) && (
+          <img src={Toppings[8].src.src} alt="Classic Sauce" className={styles.img} />
+        )
+      }
+      {defaultToppings.includes(10) && (
+        <img src={Toppings[9].src.src} alt="BBQ Sauce" className={styles.img} />
+      )}
+
+      {
+        // Map through all toppings (except crusts and sauces) //
+        // display only selected //
+        Toppings.toReversed().map((topping) => {
+          // Skip crusts and sauces //
+          if (![9, 10, 11, 12].includes(topping.id)) {
+            return defaultToppings.includes(topping.id) ? (
+              <img
+                key={topping.id}
+                src={topping.src.src}
+                alt={topping.name}
+                className={styles.img}
+              />
+            ) : null;
+          }
+          return null;
+        })
+      }
     </div>
   );
 };
